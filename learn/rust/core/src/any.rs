b9c3d5415d1420f7ee9d3cd768109bebfb332b50
Chara-X <1719552189@qq.com>
2024-12-19 20:43:47 +0800

    Up

--- learn/rust/core/src/any.rs
+++ learn/rust/core/src/any.rs
@@ -3,7 +3,10 @@ pub trait Any: 'static {
     fn type_id(&self) -> any::TypeId;
 }
 impl dyn Any {
-    pub fn is<T: any::Any>(&self) -> bool {
+    pub fn is<T: any::Any>(&self) -> bool
+    where
+        Self: any::Any,
+    {
         let x = self as &dyn any::Any;
         // let x = self as &dyn Any;
         <dyn any::Any>::is::<T>(x)
