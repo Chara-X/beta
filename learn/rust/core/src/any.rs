d5c2e180c0113ac8ae6bae4cce1a7cea5af98f2c
魏攀6092004007 <wei.pan2@zte.com.cn>
2024-12-19 17:51:38 +0800

    Up

--- learn/rust/core/src/any.rs
+++ learn/rust/core/src/any.rs
@@ -3,9 +3,17 @@ pub trait Any: 'static {
     fn type_id(&self) -> any::TypeId;
 }
 impl dyn Any {
-    pub fn is<T: any::Any>(&self) -> bool {
-        let x = self as &dyn any::Any;
-        // let x = self as &dyn Any;
-        <dyn any::Any>::is::<T>(x)
+    pub fn is<'a, T: Any>(&'a self) -> bool
+    where
+        &'a Self: any::Any,
+    {
+        <dyn any::Any>::is::<T>(&self)
+    }
+    pub fn downcast_ref<'a, T: Any>(&'a self) -> Option<&'a T>
+    where
+        &'a Self: any::Any,
+    {
+        let x = <dyn any::Any>::downcast_ref::<T>(&self);
+        panic!()
     }
 }
