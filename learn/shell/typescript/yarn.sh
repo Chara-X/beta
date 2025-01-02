4f73a8c82798146e2ddc6aff56ed897bb350d601
Chara-X <1719552189@qq.com>
2025-01-03 06:29:32 +0800

    Up

--- learn/shell/typescript/yarn.sh
+++ learn/shell/typescript/yarn.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/bash
 
-yarn create
-yarn install
-yarn add package
 yarn upgrade
-yarn remove package
-yarn global add package
-yarn global bin
-yarn global dir
-yarn global list
+yarn run
