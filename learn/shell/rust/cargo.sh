83110d29ece988083959c76c865f200a74d8b4b8
Chara-X <1719552189@qq.com>
2025-04-18 04:25:30 +0800

    Up

--- learn/shell/rust/cargo.sh
+++ learn/shell/rust/cargo.sh
@@ -2,6 +2,7 @@
 #!/usr/bin/bash
 
 cargo install crate
+cargo uninstall crate
 cargo init
 cargo test -r --target triple --no-run -- --list --exact --show-output --test-threads 0 filter
 cargo build -r --target triple
